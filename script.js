413a1f5c6841bc1609250a98f21c692fdda28ad5
sak23042006 <s.arunkumar567567567@gmail.com>
2023-10-19 15:06:18 +0530

    commit

--- script.js
+++ script.js
@@ -12,16 +12,22 @@ console.log(localStorage);
 //Challenge 2: Get the users data stored on your browser's localStorage using the getItem() method and print the localStorage in the console.
 
 console.log(localStorage.getItem("firstName"))
-// localStorage.getItem("lastName")
-// localStorage.getItem("age")
-// localStorage.getItem("country")
-// localStorage.getItem("state")
-// localStorage.clear()
-// console.log(localStorage);
+console.log(localStorage.getItem("lastName"))
+console.log(localStorage.getItem("age"))
+console.log(localStorage.getItem("country"))
+console.log(localStorage.getItem("state"))
+console.log(localStorage);
+
 
 //Challenge 3: Using removeItem() method, remove the "state" data stored in your browser's localStorage and print the localStorage in the console.
+localStorage.removeItem("state")
+console.log(localStorage);
 
 //Challenge 4: Using clear() method to remove all the data stored in your browser's localStorage and print the localStorage in the console.
+
+localStorage.clear()
+console.log(localStorage)
+
 
 //Challenge 5: Using JSON.stringify() convert the given object into the string and print the same.
 const user = {
@@ -29,3 +35,5 @@ const user = {
   age: 25,
   skills: ['HTML', 'CSS', 'JS', 'React'],
 };
+
+console.log(JSON.stringify(user));
